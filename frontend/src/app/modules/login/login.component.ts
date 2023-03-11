9654ddfcf87deec436e4901373635352f2f2c6fb
fantonix <fantonix.web@gmail.com>
2023-03-11 16:47:51 -0400

    feat(src/app/modules/login): Creando componente login

---
+++ frontend/src/app/modules/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+
+export class LoginComponent {
+  loginForm = new FormGroup({
+    username: new FormControl("", [
+      Validators.required,
+      Validators.minLength(4),
+    ]),
+    password: new FormControl("", [
+      Validators.required,
+      Validators.minLength(8),
+      Validators.maxLength(32),
+    ]),
+  })
+}
