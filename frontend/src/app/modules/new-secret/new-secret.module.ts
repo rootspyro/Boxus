5ac7e82cc6a7b07ac9c33e88c8d2daf0e5e507fd
Javier B <javierboscan16@gmail.com>
2023-03-12 10:40:52 -0400

    adding view and logic in new-secret component

--- frontend/src/app/modules/new-secret/new-secret.module.ts
+++ frontend/src/app/modules/new-secret/new-secret.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NewSecretComponent } from './new-secret.component';
 import { NewSecretRoutingModule } from './new-secret-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [NewSecretComponent],
-  imports: [CommonModule, NewSecretRoutingModule],
+  imports: [CommonModule, NewSecretRoutingModule, ReactiveFormsModule],
 })
 export class NewSecretModule {}
