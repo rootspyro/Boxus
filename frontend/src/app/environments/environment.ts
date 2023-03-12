54e6920fe9f4b47006cfa5441c8715496bf3747e
Javier B <javierboscan16@gmail.com>
2023-03-12 12:53:22 -0400

    adding future method post and fixing uploading images

--- frontend/src/app/environments/environment.ts
+++ frontend/src/app/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
+  userID: 'f1b72fce-9277-4d25-af15-7ecd86202f04',
   supabaseUrl: 'https://tqoqrsdndvoemfyybxlg.supabase.co',
   supabaseEndpoint: 'https://tqoqrsdndvoemfyybxlg.functions.supabase.co/secrets',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRxb3Fyc2RuZHZvZW1meXlieGxnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzg1ODI3MDgsImV4cCI6MTk5NDE1ODcwOH0.0FMOme8085djxnX9PocaDtoy7jkZHllFMwRtrndYpx0',
