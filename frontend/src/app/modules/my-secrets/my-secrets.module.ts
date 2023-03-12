a63a63043d88ebc074413599bf066bb8e4ce5486
Javier B <javierboscan16@gmail.com>
2023-03-12 01:14:57 -0400

    adding secret standalone component and using in my-secrets component

--- frontend/src/app/modules/my-secrets/my-secrets.module.ts
+++ frontend/src/app/modules/my-secrets/my-secrets.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MySecretsComponent } from './my-secrets.component';
 import { MySecretsRoutingModule } from './my-secrets-routing.module';
+import { SecretComponent } from 'src/app/components/secret/secret.component';
 
 @NgModule({
   declarations: [MySecretsComponent],
-  imports: [CommonModule, MySecretsRoutingModule],
+  imports: [CommonModule, MySecretsRoutingModule, SecretComponent],
 })
 export class MySecretsModule {}
