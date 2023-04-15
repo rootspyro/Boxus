0834edd6e2c329a2aea9846dbaef109d0005c438
Javier B <javierboscan16@gmail.com>
2023-04-15 14:08:22 -0400

    creating the mock of messages and adding chat component and route

--- frontend/src/app/modules/messages/messages-routing.module.ts
+++ frontend/src/app/modules/messages/messages-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MessagesComponent } from './messages.component';
+import { ChatComponent } from './components/chat/chat.component';
 
-const routes: Routes = [{ path: '', component: MessagesComponent }];
+const routes: Routes = [
+  { path: '', component: MessagesComponent },
+  { path: ':chatId', component: ChatComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- frontend/src/app/modules/messages/messages.module.ts
+++ frontend/src/app/modules/messages/messages.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MessagesComponent } from './messages.component';
 import { MessagesRoutingModule } from './messages-routing.module';
+import { ChatComponent } from './components/chat/chat.component';
 
 @NgModule({
-  declarations: [MessagesComponent],
+  declarations: [MessagesComponent, ChatComponent],
   imports: [CommonModule, MessagesRoutingModule],
 })
 export class MessagesModule {}
