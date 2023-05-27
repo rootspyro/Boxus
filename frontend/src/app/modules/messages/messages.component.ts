caabff00f4eaf255c84af47008264321c3e5989b
Javier B <javierboscan16@gmail.com>
2023-05-26 21:11:25 -0400

    refactor the infinite scroll with @angular/cdk to ngx-infinite-scroll

--- frontend/src/app/modules/messages/messages.component.ts
+++ frontend/src/app/modules/messages/messages.component.ts
@@ -1,16 +1,31 @@
-import { Component } from '@angular/core';
-import { ChatDataSource } from './services/chats.datasource';
+import { Component, OnDestroy } from '@angular/core';
+// import { ChatDataSource } from './services/chats.datasource';
 import { ChatsService } from './services/chats.service';
+import { ChatData } from './models/Chat';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
   styleUrls: ['./messages.component.scss']
 })
-export class MessagesComponent {
-  dataSource: ChatDataSource;
+export class MessagesComponent implements OnDestroy {
+  // dataSource: ChatDataSource;
+  chatItems$: Observable<ChatData[]>;
+  private offset: number = 0;
 
   constructor(private chatsService: ChatsService) {
-    this.dataSource = new ChatDataSource(this.chatsService);
+    // this.dataSource = new ChatDataSource(this.chatsService);
+    chatsService.updateChats = this.offset;
+    this.chatItems$ = chatsService.chats$;
+  }
+
+  onScrolledMessages(): void {
+    this.offset++
+    this.chatsService.updateChats = this.offset * 10;
+  }
+
+  ngOnDestroy(): void {
+    this.chatsService.clearChatsList();
   }
 }
